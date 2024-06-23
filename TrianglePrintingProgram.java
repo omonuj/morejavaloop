e12ffe899033d171d429333b5a0640aba1137d9c
jonah odoh <jonahodoh@gmail.com>
2024-06-23 14:41:47 +0100

    triangle printing program

---
+++ TrianglePrintingProgram.java
@@ -0,0 +1,37 @@
+public class TrianglePrintingProgram {
+	public static void main(String [] args){
+
+		for (int a = 1; a <= 5; a++) {
+			for (int y = a; y <= 5; y++) {
+			System.out.print("*");
+		}
+		System.out.println("");
+		}	
+
+		
+		for (int b = 1; b <= 5; b++) {
+			for (int y = b; y <= 5; y++) {
+			System.out.print("*");
+		}
+		System.out.println("");
+		}	
+		
+		
+		for (int c = 1; c <= 5; c++) {
+			for (int y = c; y <= 5; y++) {
+			System.out.print("*");
+		}
+		System.out.println("");
+		}	
+
+		
+		for (int d = 1; d <= 5; d++) {
+			for (int y = d; y <= 5; y++) {
+			System.out.print("*");
+		}
+		System.out.println("");
+		}	
+		
+
+	}
+}
