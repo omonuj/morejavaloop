1516f303ecce8c52dcc7306affa082d0318b05b6
jonah odoh <jonahodoh@gmail.com>
2024-06-27 15:26:53 +0100

    switched

---
+++ Jonah.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+public class Jonah {
+
+	public static void main(String [] args){
+	Scanner scanner = new Scanner(System.in);
+	int user = 0;
+
+	while (user = 1; user <= 6) {
+	System.out.print("Enter a number: " );
+	user = scanner.nextInt();
+	user++;
+
+	if (user == 1) {
+	System.out.println("To make transfer");
+	}
+	else if (user == 2) {
+	System.out.println("To buy data");
+	}
+	else if (user == 3) {
+	System.out.println("To can open an account");
+	}
+	else if (user == 4) {
+	System.out.println("For insurance");
+	}
+	else if (user == 5) {
+	System.out.println("To pay bills");
+	}
+	else if (user == 6) {
+	System.out.println("To terminate");
+
+	}
+	}
+}
+	
+}
+
+
+
+
+
